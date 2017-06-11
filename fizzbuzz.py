627a632708b5d02bff80558595ae0bb12fd83feb
alaragebes <alaragb@gmail.com>
2017-06-11 13:51:42 +0300

    another exercise

---
+++ fizzbuzz.py
@@ -0,0 +1,24 @@
+for i in range(1, 101) :
+    if i % 15 == 0 :
+        print "FizzBuzz"
+    elif i % 3 == 0 :
+        print "Fizz"
+    elif i % 5 == 0 :
+        print "Buzz"
+    else :
+        print i
+
+
+
+##def find_numbers (self) :
+     # for num in self.elements :
+          #if num == 3 or num % 3 == 0 :
+            #  self.items = 'Fizz'
+             # self.elements[int(num)] = self.items
+        #  elif num == 5 or num % 5 == 0 :
+              #self.items = 'Buzz'
+              #self.elements[int(num)] = self.items
+          #elif num % 3 == 0 and num % 5 == 0 :
+            #  self.items = 'FizzBuzz'
+             # self.elements[int(num)] = self.items
+     # print self.elements
